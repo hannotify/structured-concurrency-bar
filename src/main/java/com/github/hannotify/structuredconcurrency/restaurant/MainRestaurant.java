93dff6cca2868dc914bc1cb47d53fd39bf65c7f2
Hanno Embregts <hanno.embregts@infosupport.com>
2023-10-24 21:29:50 +0200

    Created StructuredConcurrencyRestaurant

--- src/main/java/com/github/hannotify/structuredconcurrency/restaurant/MainRestaurant.java
+++ src/main/java/com/github/hannotify/structuredconcurrency/restaurant/MainRestaurant.java
@@ -1,12 +1,8 @@
 package com.github.hannotify.structuredconcurrency.restaurant;
 
-import com.github.hannotify.structuredconcurrency.restaurant.kitchen.OutOfStockException;
-
-import java.util.concurrent.ExecutionException;
-
 public class MainRestaurant {
     public static void main(String[] args) throws Exception {
-        Restaurant restaurant = new SingleWaiterRestaurant();
+        Restaurant restaurant = new StructuredConcurrencyRestaurant();
         restaurant.announceMenu();
     }
 }
