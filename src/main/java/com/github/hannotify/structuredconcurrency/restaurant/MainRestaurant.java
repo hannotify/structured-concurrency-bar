55268fa33de97218417d192ad32bcb618701568d
Hanno Embregts <hanno.embregts@infosupport.com>
2025-08-18 16:39:45 +0200

    Reset to start of demo

--- src/main/java/com/github/hannotify/structuredconcurrency/restaurant/MainRestaurant.java
+++ src/main/java/com/github/hannotify/structuredconcurrency/restaurant/MainRestaurant.java
@@ -1,10 +1,10 @@
 import com.github.hannotify.structuredconcurrency.restaurant.Restaurant;
-import com.github.hannotify.structuredconcurrency.restaurant.StructuredConcurrencyRestaurant;
+import com.github.hannotify.structuredconcurrency.restaurant.SingleWaiterRestaurant;
 import com.github.hannotify.structuredconcurrency.restaurant.kitchen.OutOfStockException;
 
 import java.util.concurrent.ExecutionException;
 
 void main() throws ExecutionException, InterruptedException, OutOfStockException {
-    Restaurant restaurant = new StructuredConcurrencyRestaurant();
+    Restaurant restaurant = new SingleWaiterRestaurant();
     restaurant.announceMenu();
 }
