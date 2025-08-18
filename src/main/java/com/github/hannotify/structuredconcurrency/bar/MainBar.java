5a1a82053080dd35220d24fd891f91846bfcecb6
Hanno Embregts <hanno.embregts@infosupport.com>
2025-08-18 16:46:17 +0200

    Create a StructuredConcurrencyBar

--- src/main/java/com/github/hannotify/structuredconcurrency/bar/MainBar.java
+++ src/main/java/com/github/hannotify/structuredconcurrency/bar/MainBar.java
@@ -2,13 +2,13 @@ import com.github.hannotify.structuredconcurrency.bar.Bar;
 import com.github.hannotify.structuredconcurrency.bar.Drink;
 import com.github.hannotify.structuredconcurrency.bar.DrinkCategory;
 import com.github.hannotify.structuredconcurrency.bar.Guest;
-import com.github.hannotify.structuredconcurrency.bar.MultiWaiterBar;
+import com.github.hannotify.structuredconcurrency.bar.StructuredConcurrencyBar;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 void main() throws InterruptedException, ExecutionException {
-    Bar bar = new MultiWaiterBar();
+    Bar bar = new StructuredConcurrencyBar();
 
     Guest hanno = new Guest("Hanno", List.of(
             new Drink("Espresso", DrinkCategory.COFFEE),
