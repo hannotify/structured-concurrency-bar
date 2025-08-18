ee2bcd7f173135a301c12d262357160aaba409f6
Hanno Embregts <hanno.embregts@infosupport.com>
2025-08-18 16:42:15 +0200

    Create a StructuredConcurrencyRestaurant

---
+++ src/main/java/com/github/hannotify/structuredconcurrency/restaurant/StructuredConcurrencyRestaurant.java
@@ -0,0 +1,28 @@
+package com.github.hannotify.structuredconcurrency.restaurant;
+
+import com.github.hannotify.structuredconcurrency.restaurant.kitchen.CourseType;
+import com.github.hannotify.structuredconcurrency.restaurant.kitchen.MultiCourseMeal;
+import com.github.hannotify.structuredconcurrency.staff.Waiter;
+
+import java.util.concurrent.StructuredTaskScope;
+
+public class StructuredConcurrencyRestaurant implements Restaurant {
+    @Override
+    public MultiCourseMeal announceMenu() throws InterruptedException {
+        Waiter grover = new Waiter("Grover");
+        Waiter zoe = new Waiter("Zoe");
+        Waiter rosita = new Waiter("Rosita");
+
+        try (var scope = StructuredTaskScope.open()) {
+            var starter = scope.fork(() -> grover.announceCourse(CourseType.STARTER));
+            var main = scope.fork(() -> zoe.announceCourse(CourseType.MAIN));
+            var dessert = scope.fork(() -> rosita.announceCourse(CourseType.DESSERT));
+
+            scope.join();
+
+            return new MultiCourseMeal(starter.get(),
+                                       main.get(),
+                                       dessert.get());
+        }
+    }
+}
